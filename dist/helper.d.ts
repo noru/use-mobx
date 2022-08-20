e8fb042ee43445a3a166cec4f8f0e4ef4c91929e
noru <nooooru@gmail.com>
2022-08-20 18:42:28 +0800

    clean up

--- dist/helper.d.ts
+++
@@ -1,4 +0,0 @@
-import { useEffect } from 'react';
-export declare const useUpdateEffect: typeof useEffect;
-export declare function useRerender(): () => void;
-export declare function debounceUpdate(updater: any, callback: any): void;
--- dist/useMultiObservable.d.ts
+++
@@ -1,8 +0,0 @@
-import { Store } from './useObservable';
-/**
- *
- * @param stores
- * @returns
- * @category Hooks
- */
-export declare function useMultiObservable<T extends Array<Store>>(...stores: T): T;
