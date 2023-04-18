7356208dc3ca51219491549f1a9fed28749c5934
noru <nooooru@gmail.com>
2023-04-18 14:16:25 +0800

    temp fix for components that shouldn't be wrapped

--- auto-wrap-observer/wrap-jsx.js
+++ auto-wrap-observer/wrap-jsx.js
@@ -1,18 +1,35 @@
 /* eslint-disable */
 'use-strict';
 
-globalThis['__auto_wrap_mobx_observer_hoc__'] = true
-
 var observer
 try {
   observer = require('mobx-react-lite').observer
 } catch (error) {
-  console.error(`[use-mobx] mobx-react-lite is not installed. Please install it to use the auto wrap feature.`, error);
+  console.error(`[use-mobx] mobx-react-lite is not installed. Please install it to use the auto wrap feature.`, error)
+  throw error
 }
 
+globalThis['__auto_wrap_mobx_observer_hoc__'] = true
+
+var _cache = new WeakMap()
 var wrapJsx = (jsx) => (type, ...args) => {
-  var observed = typeof type === 'function' ? observer(type) : type
-  return jsx(observed,...args)
+
+  if (typeof type === 'function') {
+    switch(true) {
+      case type.$$tyoeof: // skip internal react components: memo/forwardRef/provider...
+        break
+      case type.prototype && 'render' in type.prototype: // class component not supported
+        break
+      case type.name === 'Route': // skip react-router
+        break
+      default:
+        if (!_cache.has(type)) {
+          _cache.set(type, observer(type))
+        }
+        type = _cache.get(type)
+    }
+  }
+  return jsx(type,...args)
 }
 
 module.exports = wrapJsx
